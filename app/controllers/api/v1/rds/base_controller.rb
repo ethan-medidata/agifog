c3d94dda34293b134619461154eff2c4ab7b2e9c
Rodrigo Estebanez <restebanez@mdsol.com>
2011-12-22 14:07:37 +0100

    adding rds_default_server_params

--- app/controllers/api/v1/rds/base_controller.rb
+++ app/controllers/api/v1/rds/base_controller.rb
@@ -22,4 +22,13 @@ class Api::V1::Rds::BaseController < ActionController::Base
     @rds ||= Fog::AWS::RDS.new
   end
   
+  def rds_default_server_params
+    {
+      :engine_version => "5.1.57", 
+      :allocated_storage => 5, 
+      :engine => 'mysql',
+      :flavor_id => "db.m1.small", 
+      :backup_retention_period => 0 
+    }
+  end
 end
