6331b56d6c200ebe5377eb75761208a16813a9db
Rodrigo Estebanez <restebanez@mdsol.com>
2012-03-07 14:54:01 +0100

    auto_scaling/configurations fully implemented with rspecs and fog contribution

--- app/controllers/api/v1/auto_scaling/configurations_controller.rb
+++ app/controllers/api/v1/auto_scaling/configurations_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AutoScaling::ConfigurationsController < Api::V1::AutoScaling::BaseController
+  before_filter :load_params_parsed, :only => [:create]
   
   # http://docs.amazonwebservices.com/AutoScaling/latest/APIReference/API_DescribeLaunchConfigurations.html
   def index
@@ -6,7 +7,7 @@ class Api::V1::AutoScaling::ConfigurationsController < Api::V1::AutoScaling::Bas
       if @configurations = as.configurations
         pretty_json_render(@configurations)
       else
-        error = { :errors => ["There was a problem retrieving the parameter groups"]}
+        error = { :errors => ["There was a problem retrieving the launch configurations"]}
         pretty_json_render(error, 404)
       end
     rescue => e
@@ -15,5 +16,46 @@ class Api::V1::AutoScaling::ConfigurationsController < Api::V1::AutoScaling::Bas
     end
   end
   
-
+  def show
+    begin
+      if @configuration = as.configurations.get(params[:id])
+        pretty_json_render(@configuration)
+      else
+        error = { :errors => ["#{params[:id]} launch configuration not found"] }
+        pretty_json_render(error, 404)
+      end
+    rescue => e
+      error =  { :errors => [e.message.to_json] }
+      pretty_json_render(error, 422)
+    end
+  end
+    
+  def create
+    begin
+      @configuration = as.configurations.create(@params_parsed["configuration"])
+      if @configuration
+        pretty_json_render(@configuration)
+      else
+        error = { :errors => [@configuration.errors] }
+        pretty_json_render(error, 400)
+      end
+    rescue => e
+      rescued_pretty_json_render(e,422)
+    end
+  end
+  
+  def destroy
+    begin
+      @configuration = as.configurations.get(params[:id])
+      raise "#{params[:id]} doesn't exist" unless @configuration
+      if @configuration.destroy
+        pretty_json_render(["#{params[:id]} was deleted successfully"])
+      else        
+        error = { :errors => ["#{params[:id]} wasn't deleted"]}
+        pretty_json_render(error, 404)
+      end
+    rescue => e
+      rescued_pretty_json_render(e,422)
+    end
+  end
 end
--- spec/api/v1/auto_scaling/configurations_spec.rb
+++ spec/api/v1/auto_scaling/configurations_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 
 describe "/api/v1/auto_scaling/configurations", :type => :api do
+  before(:all) do
+    @image = compute.register_image('image', 'image', '/dev/sda1').body
+    @image_id = @image['imageId']
+  end
+  
   it "should return nothing when there isn't any configurations" do
     get '/api/v1/auto_scaling/configurations'
     last_response.should be_ok
@@ -12,21 +17,93 @@ describe "/api/v1/auto_scaling/configurations", :type => :api do
     
     before(:all) do
       @launch_configuration = "rspec-test-configuration"
-      @image = compute.register_image('image', 'image', '/dev/sda1').body
-      @image_id = @image['imageId']
       lc = as.configurations.create(:id => @launch_configuration, :image_id => @image_id, :instance_type => 't1.micro')
+      lc = as.configurations.create(:id => 'second-launch-configuration', :image_id => @image_id, :instance_type => 't1.micro')
     end
     
     it "should list every launch configuration" do
       get '/api/v1/auto_scaling/configurations'
       last_response.should be_ok
       attributes = JSON.parse(last_response.body)
-      attributes.first['id'].should == @launch_configuration
-      attributes.first['image_id'].should == @image_id
-      attributes.first['instance_type'].should == 't1.micro'
-      attributes.first['security_groups'].should == []
+      attributes.select do |lc|
+        lc['id'].should == @launch_configuration
+        lc['image_id'].should == @image_id
+        lc['instance_type'].should == 't1.micro'
+        lc['security_groups'].should == []
+      end
     end
-
+    
+    it "should show a launch configuration" do
+      get "/api/v1/auto_scaling/configurations/#{@launch_configuration}"
+      last_response.should be_ok
+      attribute = JSON.parse(last_response.body)
+      attribute['id'].should == @launch_configuration
+      attribute['image_id'].should == @image_id
+      attribute['instance_type'].should == 't1.micro'
+      attribute['security_groups'].should == []
+    end
+    
+    it "should return 404 when a launch configuration doesn't exist" do
+      get "/api/v1/auto_scaling/configurations/dontexist.json"
+      last_response.should_not be_ok
+      last_response.status.should == 404
+      attributes = JSON.parse(last_response.body)
+      attributes["errors"].should == ["dontexist launch configuration not found"]
+    end
+  end #describe listing
+  
+  describe "create launch configurations" do
+    before(:all) do
+      @other_launch_configuration = "rspec-create-configuration"
+    end
+    
+    it "should create a launch configuration" do
+      post "/api/v1/auto_scaling/configurations", {:configuration => { 
+          :id => @other_launch_configuration,
+          :image_id => @image_id,
+          :instance_type => "m1.small"}
+        }.to_json
+      last_response.status == 201
+      attributes = JSON.parse(last_response.body)
+      attributes['id'].should == @other_launch_configuration
+      attributes['image_id'].should == @image_id
+      attributes['instance_type'].should == 'm1.small'
+      
+      # verify that it was created
+      get "/api/v1/auto_scaling/configurations/#{@other_launch_configuration}"
+      last_response.should be_ok
+      attribute = JSON.parse(last_response.body)
+      attribute['id'].should == @other_launch_configuration
+      attribute['image_id'].should == @image_id
+      attribute['instance_type'].should == 'm1.small'
+    end
+    
+    
+    it "should fail if all the required params aren't passed" do
+      post "/api/v1/auto_scaling/configurations", {:configuration => { 
+          :id => @other_launch_configuration,
+          :instance_type => "m1.small"}
+        }.to_json
+      last_response.status == 400
+      attributes=JSON.parse(last_response.body)
+      attributes["errors"].should == ["image_id is required for this operation"]
+    end
+  end #describe create
+  
+  describe "should delete launch configurations" do
+    before(:all) do
+      @launch_configuration_delete = "rspec-delete-configuration"
+      lc = as.configurations.create(:id => @launch_configuration_delete, :image_id => @image_id, :instance_type => 't1.micro')
+    end
+    
+    it "delete an existing launch configuration" do
+      delete "/api/v1/auto_scaling/configurations/#{@launch_configuration_delete}"
+      last_response.should be_ok
+      # verify that it was deleted
+      get "/api/v1/auto_scaling/configurations/#{@launch_configuration_delete}"
+      last_response.should_not be_ok
+      last_response.status.should == 404
+    end
   end
 end
 
