356dc57c660792f95e52af6ddc2133b8fdf1f7ee
Rodrigo Estebanez <restebanez@mdsol.com>
2012-06-07 15:54:54 +0200

    Move skip_before_filter :verify_authenticitiy_token to application_controller.rb

--- app/controllers/api/v1/iam/base_controller.rb
+++ app/controllers/api/v1/iam/base_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::Iam::BaseController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  
-  def iam
-    @iam ||= Fog::AWS::IAM.new
-  end
-  
-  
-  
+ 
+ private  
+   def iam
+     @iam ||= Fog::AWS::IAM.new
+   end
+   
+   
+   def load_user
+     @user = iam.users.get(params[:user_id])
+   end
+   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  skip_before_filter :verify_authenticity_token
+  
   
   respond_to :json
   
